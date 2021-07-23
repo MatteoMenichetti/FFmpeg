5739fef59690a907f93b03281f4ae3bbbc0902a8
matteo <matteo.menichetti@stud.unifi.it>
2021-07-23 18:45:03 +0200

    second commit

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() {
+int main(int argv, char *argc[]) {
+    if (argv < 2) {
+        printf("Number of arguments < 2\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("argc[1] =  %s\n", argc[1]);
+    if (!fork()) {
+        execlp("find", "find", argc[1], "-type", "f", "-name", "*.m*", "|", "cat", ">", "name.txt", NULL);
+    }
+
     printf("Hello, World!\n");
     return 0;
 }
