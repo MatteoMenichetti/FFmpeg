84375dcbb55bc00b24e0037af60cdde66c45a784
matteo <matteo.menichetti@stud.unifi.it>
2021-07-25 18:05:01 +0200

    first read of directory with linked list

--- main.c
+++ main.c
@@ -2,17 +2,44 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+struct LinkedList {
+    char *token;
+    struct LinkedList *next;
+};
+struct LL {
+    struct LinkedList *this;
+    struct LL *next;
+};
 
 int main(int argv, char *argc[]) {
-    if (argv < 2) {
-        printf("Number of arguments < 2\n");
-        exit(EXIT_FAILURE);
-    }
-    printf("argc[1] =  %s\n", argc[1]);
     if (!fork()) {
-        execlp("find", "find", argc[1], "-type", "f", "-name", "*.m*", "|", "cat", ">", "name.txt", NULL);
+        execlp("bash", "bash", "../bash.sh", (char *) NULL);
     }
-
+    FILE *fp = fopen("name.txt", "r");
+    struct LinkedList *first, *actual;
+    struct LL *list = calloc(1, sizeof(struct LL)), *firstLL=list;
+    char line[100];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        first = (struct LinkedList *) calloc(1, sizeof(struct LinkedList));
+        actual = first;
+        printf("first assignment\n");
+        actual->token = strtok(line, "/");
+        actual->next = (struct LinkedList *) calloc(1, sizeof(struct LinkedList));
+        actual = actual->next;
+        while ((actual->token = strtok(NULL, "/")) != NULL) {
+            printf("next assignment\n");
+            actual->next = (struct LinkedList *) calloc(1, sizeof(struct LinkedList));
+            actual = actual->next;
+        }
+        free(actual);
+        list->this = first;
+        list->next = (struct LL *) calloc(1, sizeof(struct LL));
+    }
+    for (firstLL = firstLL; firstLL != NULL; firstLL = firstLL->next)printf("%s, ", firstLL->this);
     printf("Hello, World!\n");
     return 0;
 }
